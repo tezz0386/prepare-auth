ef04b2500357261c5cb2a4daca2b99aab4bbda20
tezz0386 <dangaura.tejendra.123@gmail.com>
2021-08-27 13:37:23 +0545

    First level auth

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Auth;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, String $role)
+    {
+        $user = Auth::user();
+        if($user->role == $role)
+        return $next($request);
+        return redirect('/');
+    }
+}
